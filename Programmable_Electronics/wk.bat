b047ad41e7e26ff10842700266ec8fa64b58b750
J.L. Noullet <noullet@chipyards.com>
2024-04-27 20:00:48 +0200

    version 2024: pagination pour PDF revue, page-break-inside au lieu de page-break-before, script wk.bat pour print avec wkhtmltopdf, TOC

---
+++ Programmable_Electronics/wk.bat
@@ -0,0 +1,3 @@
+rem F:\Appli\wkhtmltox32\bin\wkhtmltopdf.exe
+rem F:\Appli\wkhtmltox64\bin\wkhtmltopdf.exe
+N:\Appli\wkhtmltox\bin\wkhtmltopdf.exe --print-media-type --enable-local-file-access --dpi 300 -T 10 -R 10 -B 17 -L 10 --footer-center "[page]" --footer-spacing 2 --footer-font-size 10 1_Introduction.html 2_The_Analogies.html 3_Circuit_Simulation.html 4_The_Oscilloscope.html 5_The_Microcontroller.html 6_The_Feedback.html toc A1-6t.pdf
